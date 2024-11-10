ceb0555e2cf315fe1dc36cc77e49ab24274418e0
DevMatheusSilva <matheusmello225@gmail.com>
2024-11-10 15:26:13 -0300

    feat: criada strategie para inativacao de cadastro

---
+++ src/strategies/impls/ClienteImpls/Inativar.ts
@@ -0,0 +1,22 @@
+import IStrategy from "../../IStrategy";
+import Entidade from "../../../entities/Entidade";
+import Cliente from "../../../entities/Cliente";
+
+export default class Inativar implements IStrategy {
+    public processar(entidade: Entidade) {
+        if (entidade instanceof Cliente) {
+            entidade.estaAtivo = false;
+
+            for (const endereco of entidade.enderecos) {
+                endereco.estaAtivo = false;
+            }
+
+            for (const cartao of entidade.cartoes) {
+                cartao.estaAtivo = false;
+            }
+
+            entidade.telefone.estaAtivo = false;
+            entidade.usuario.estaAtivo = false;
+        }
+    }
+}
